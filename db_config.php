c984c1f1b9bf4d2b3ff2d19b468ceb8bffc11594
tsmilan <101035074@student.swin.edu.au>
2018-09-20 23:21:19 +1000

    add: database configuration file
    Database credentials must be supplied here

---
+++ db_config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Database configuration file.
+ *
+ * @author Trisha Milan <tshmilan@gmail.com>
+ */
+
+$config = array(
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+    'dbname' => '',
+    'port' => '',
+    'socket' => ''
+);
