b47fda450a1166bb48f681f8bba730d96c3cad12
tsmilan <101035074@student.swin.edu.au>
2018-09-24 15:16:22 +1000

    chore: delete some files and update user_upload.php
    This commit will delete some files in the repository that has been moved to the app folder.

--- user_upload.php
+++ user_upload.php
@@ -5,12 +5,12 @@
  * @author Trisha Milan <tshmilan@gmail.com>
  */
 
-require_once('constants.php');
-require_once('functions.php');
-require_once('Database.php');
-require_once('DatabaseQuery.php');
-require_once('Commands.php');
-require_once('User.php');
+require_once('app/constants.php');
+require_once('app/functions.php');
+require_once('app/db/Database.php');
+require_once('app/db/DatabaseQuery.php');
+require_once('app/Commands.php');
+require_once('app/model/User.php');
 
 $cmd = new Commands();
 $cmd->readCommandLineArgs($argv);
